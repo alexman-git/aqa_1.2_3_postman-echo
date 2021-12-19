d6440f37bf2817f7637eabda54b39569b56dfdae
Alexander Maletin <man.alex212@gmail.com>
2021-12-19 16:06:25 +0500

    Broke PostmanEchoTest

--- src/test/java/ru/netology/rest/PostManEchoTest.java
+++ src/test/java/ru/netology/rest/PostManEchoTest.java
@@ -20,6 +20,6 @@ public class PostManEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some value"));
     }
 }
