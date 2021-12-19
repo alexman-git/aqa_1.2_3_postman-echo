91810e6a7818594c8660daea5386cf59c7e5c96e
Alexander Maletin <man.alex212@gmail.com>
2021-12-19 16:00:27 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/k2608i70x05l01t6?svg=true)](https://ci.appveyor.com/project/alexman-git/aqa-1-2-3-postman-echo)
---
+++ src/test/java/ru/netology/rest/PostManEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostManEchoTest {
+
+    @Test
+    void shouldPostAndCheckBody() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
